165765ef48781074c66a2828372c64c9545451b8
VeraKH <vera_khatskevich@sibmail.com>
2015-12-17 14:57:19 +0300

    git page

---
+++ admin.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">                      
+    <link href="css/bootstrap.min.css" rel="stylesheet" />    
+    <link href="css/screen.css" rel="stylesheet" type="text/css" medea= "screen"/>            
+    <script src="js/bootstrap.min.js"></script>  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>          
+</head>
+<body>
+
+
+
+<div class="container-fluid">
+
+    <div class="row"> 
+	    <div class="col-md-9" id="header-name">
+	    	<h2>JClass</h2>
+    	    </div>
+    	    <div class="col-md-3" id = "header-nav">
+	    	<ul>
+	    		<li><a href="logout.php">Logout</a></li>
+	    	</ul>
+    	    </div>
+    </div>
+
+    <div class="row">
+    	<div class="col-md-3" id = "side-menu-nav">
+    	<h3>Admin Menu</h3>
+    	<p>Welcome to admin area</p>
+	    	<ul>
+	    		<li><a href="manage_content.php">Manage Website Content</a></li>
+	    		<li><a href="manage_content.php">Manage Admin Users</a></li>
+	    		<li><a href="manage_content.php">Manage Users</a></li>
+	    	</ul>
+    	    </div>
+    	    <div class="col-md-9" id="content-area">
+	    	<h4>Articles</h4>
+    	    </div>
+    </div>
+                        <hr class="small">
+<div class="row">
+
+       <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h4>JClass</h4>
+                    <p>Moscow Kremlin<br>Russia</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:vera_khatskevich@yahoo.com">vera_khatskevich@yahoo.com</a></li>
+                    </ul>
+                    <br>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="https://www.facebook.com/vera.khatskevich1"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-dribbble fa-fw fa-3x"></i></a>
+                        </li>
+                    </ul>
+                    <p class="text-muted">Copyright LEARNIT 2015</p>
+      </div>
+
+</div>
+
+
+</div>
+
+
+</body>
+</html>
---
+++ manage_content.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JClass</title>
+        <meta charset="UTF-8">
+    </head>
+<body>
+
+</body>
+</html>
